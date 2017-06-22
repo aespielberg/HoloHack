a2aeded748ddfd56d9d8120af508b4f6d5cf28e3
Andrew Spielberg <aespielb@mit.edu>
2017-06-22 14:37:33 -0400

    Almost working scene transitions

---
+++ EmpathyTransition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class EmpathyTransition : MonoBehaviour
+{
+
+    public Button empathy;
+
+    // Use this for initialization
+    void Start()
+    {
+        empathy = gameObject.GetComponent<Button>();
+        empathy.onClick.AddListener(EmpathyTransitionClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void EmpathyTransitionClick()
+    {
+        SceneManager.LoadScene("BallDemoScene", LoadSceneMode.Single);
+    }
+}
